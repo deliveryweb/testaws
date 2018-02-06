6eb249b70fd7f30a316393f7a7db9b94e17c98e9
Андрей Зубков < >
2018-02-07 03:04:23 +0500

    fix

--- function1/index.js
+++ function1/index.js
@@ -1,24 +1,6 @@
 'use strict';
 const AWS = require('aws-sdk');
-const docClient = new AWS.DynamoDB.DocumentClient({region: 'us-west-1'});
 
 exports.handler = (event, context, callback) => {
-    
-    // let companyId = event.companyId
-    let companyId = "123xyz";
-    var params = {
-      TableName: 'Groups',
-      KeyConditionExpression: 'companyId = :cid',
-      ExpressionAttributeValues: {
-        ':cid': companyId,
-      }
-    };
-    docClient.query(params, function(err, data) {
-        if(err){
-            callback(err, null)
-        } else {
-            var items = data.Items;
-            callback(err, items)
-        }
-    })
+  console.log('function1');  
 };
--- function2/index.js
+++ function2/index.js
@@ -3,22 +3,5 @@ const AWS = require('aws-sdk');
 const docClient = new AWS.DynamoDB.DocumentClient({region: 'us-west-1'});
 
 exports.handler = (event, context, callback) => {
-    
-    // let companyId = event.companyId
-    let companyId = "123xyz";
-    var params = {
-      TableName: 'Groups',
-      KeyConditionExpression: 'companyId = :cid',
-      ExpressionAttributeValues: {
-        ':cid': companyId,
-      }
-    };
-    docClient.query(params, function(err, data) {
-        if(err){
-            callback(err, null)
-        } else {
-            var items = data.Items;
-            callback(err, items)
-        }
-    })
+    console.log('function2'); 
 };
